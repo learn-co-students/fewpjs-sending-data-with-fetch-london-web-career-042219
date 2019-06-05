c02b2241a35ea424e1db3ed0da8e22d46550bbeb
MarcoMiranda05 <marmirand@hotmail.com>
2019-06-05 13:03:18 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+
+ function submitData (userName, userEmail){
+  return fetch ("http://localhost:3000/users",{ 
+      method: "POST",
+      headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name: userName,
+        email: userEmail
+      })
+  })
+ .then(function(response) {
+      return response.json();
+  }) 
+  .then(function(object) {
+      const newId = document.createElement('li')
+      document.body.appendChild(newId)
+      newId.innerHTML = object.id
+  })
+  .catch(function(error) {
+      const newError = document.createElement('li')
+      document.body.appendChild(newError)
+      newError.innerHTML = error.message
+  }) 
+}

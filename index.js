7dd12bc3c0c2f28cc14cfea22c0f648ff2e2864e
AlisherZakir <a3zakir@gmail.com>
2019-06-24 18:10:09 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+
+function submitData(name , email) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+  .then(resp => resp.json())
+  .then(({id}) => document.body.append(id))
+  .catch(({message}) => document.body.append(message));
+}
